24f2a9673151a15cfb4e37e3c207cba592661493
Markowian22 <120415395+Markowian22@users.noreply.github.com>
2024-02-10 23:09:56 +0100

    changed webv

--- src/WEB_SCRAPER_EASY/Easy_Web_Scraping.py
+++ src/WEB_SCRAPER_EASY/Easy_Web_Scraping.py
@@ -18,16 +18,6 @@ from .locator_dict import LOCATOR_DICT
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# LOCATOR_DICT = {
-#     "xpath": By.XPATH,
-#     "id": By.ID,
-#     "class_name": By.CLASS_NAME,
-#     "css_selector": By.CSS_SELECTOR,
-#     "tag_name": By.TAG_NAME,
-#     "link_text": By.LINK_TEXT,
-#     "name": By.NAME,
-# }
-
 
 class Connector:
     """
